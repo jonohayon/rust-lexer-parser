edab098179f744ccae501ac7ac30fb489ad38746
Jonathan Ohayon <me@jonathano.com>
2019-09-15 13:56:09 +0300

    Created TokenType, Token data structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug, PartialEq)]
+enum TokenType {
+    Comma,
+    Number,
+    Identifier,
+    Whitespace, Newline, EOF
+}
+
+#[derive(Debug)]
+struct Token {
+    kind: Option<TokenType>,
+    data: Option<String>,
+    start: Option<usize>,
+    position: Option<(usize, usize)>
+}
 fn main() {
     println!("Hello, world!");
 }
